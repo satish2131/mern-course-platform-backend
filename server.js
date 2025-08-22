59d31eaeae1d17cac9ac8fcc945fc866d7f48986
satish2131 <satishcm035@gmail.com>
2025-08-22 20:16:55 +0530

    Fix path-to-regexp issue and set Node 18

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use("/api/certificates", require("./routes/certificates"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend", "build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 }
